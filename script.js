408812b65eabfc3fe8268d869a40075396de48dc
Priscila Schleske <plasch7@gmail.com>
2023-12-09 23:14:10 -0700

    adds homepage, starts drinkware submenu

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('Hello, world!')
+
+function toggleArrow() {
+    const arrowLink = document.querySelector('.arrow-link');
+    arrowLink.classList.toggle('clicked');
+}
+
+function toggleArrow2() {
+    const arrowLink2 = document.querySelector('.arrow-link2');
+    arrowLink2.classList.toggle('clicked');
+}
