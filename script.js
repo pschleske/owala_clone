cd41d62f50bc86cc854ebe027c12d4dfe791e1aa
Priscila Schleske <plasch7@gmail.com>
2023-12-09 23:36:56 -0700

    toggles drinkware menu

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ function toggleArrow2() {
     const arrowLink2 = document.querySelector('.arrow-link2');
     arrowLink2.classList.toggle('clicked');
 }
+
+let subMenu = document.getElementById("subMenu")
+function toggleMenu() {
+    subMenu.classList.toggle("open-menu")
+}
